ff11d14cc147b1775e201eb6c113e3d6dd9f44e1
snyper2121 <gaby4b11@gmail.com>
2016-10-15 18:12:53 -0400

    Project finished

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 $(document).foundation()
 
-var contactList = {};
+var contactList = [];
 
 
-
-$( "#submitButton" ).on('click',function() {
+$( "#submit-button" ).on('click',function() {
   	var newContactObject = {
 		fName: document.getElementById('fName').value,
 		lName: document.getElementById('lName').value,
@@ -15,21 +14,42 @@ $( "#submitButton" ).on('click',function() {
 	};
 
   	contactList.push(newContactObject);
-  	contactListRender = document.getElementById('contactList');
+  	console.log(contactList);
+  	contactListRender = document.getElementById('contact-list');
 
-  //	var listString = "";
-
-	for (var i = 0; i < contactList.length; i++) {
-		var listString = "";
-		listString = listString +  "<li id="+i+"><a>" + contactDisplayName(contactList[i]) + "</a></li>";
-	};
-
+  	renderContactList();
+  	
+  	document.getElementById("fName").value = "";
 });
 
+
+function renderContactList() {
+	var listString = "";
+	for (var i = 0; i < contactList.length; i++) {
+		listString = listString +  "<li class='contact'><a id="+i+">" + contactDisplayName(contactList[i]) + "</a></li>";
+	};
+	document.getElementById("contact-list").innerHTML = listString;
+
+	$('.contact').on('click', function(event) {
+		console.log(event.target.id);
+		console.log(contactList[Number(event.target.id)].fName);
+
+		document.getElementById("display-info").innerHTML = 
+		"<ul>"
+			+ "<li>First Name:" + (contactList[Number(event.target.id)].fName) + "</li>"
+			+ "<li>Last Name:" + (contactList[Number(event.target.id)].lName) + "</li>"
+			+ "<li>Phone Number:" + (contactList[Number(event.target.id)].phoneNumber) + "</li>"
+			+ "<li>Street Name:" + (contactList[Number(event.target.id)].streetName) + "</li>"
+			+ "<li>City Name:" + (contactList[Number(event.target.id)].cityName) + "</li>"
+			+ "<li>State Name:" + (contactList[Number(event.target.id)].stateName) + "</li>" +
+		"</ul>";
+});
+
+}
 
 function contactDisplayName(contactObj) {
 	return contactObj.fName + " " + contactObj.lName;
 }
 
 
-function 
+
